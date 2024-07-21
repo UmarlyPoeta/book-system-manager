8acb398e9567421c8774d866f8139ed1d2b57818
UmarlyPoeta <pkozlowski@student.agh.edu.pl>
2024-07-21 16:50:40 +0200

    added simple cli menu

--- main.py
+++ main.py
@@ -1,31 +1,62 @@
 from book import Book
 from library import Library
+import os
 
+def main_menu():
+    print("=== Book System Manager ===")
+    print("[1] Load data from file")
+    print("[2] Save data to file")
+    print("[3] Exit")
+    print("[4] Add book")
+    print("[5] Show available books")
+    print("[6] Rent book")
+    print("[7] Return book")
+    print("[8] Help")
+    print("[9] About")
+
+def main():
+    library = Library()
+
+    while True:
+        main_menu()
+        choice = input("Choose an option: ")
+
+        match choice:
+            case "1":
+                library.load_from_file("library_data.json")
+                print("library was loaded")
+            case "2":
+                library.save_to_file("library_data.json")
+                print("library was saved")
+            case "3":
+                break
+            case "4":
+                title = input("Enter book title: ")
+                author = input("Enter book author: ")
+                year = int(input("Enter release year: "))
+                isbn = input("Enter ISBN: ")
+                text_file_name = input("Enter text file name: ")
+                book = Book(title, author, year, isbn, text_file_name)
+                library.add_book(book)
+                print("Book was added")
+            case "5":
+                for book in library.book_list:
+                    if not book.rent:
+                        print(book)
+            case "6":
+                isbn = input("Enter ISBN of the book to rent: ")
+                print(library.rent_book(isbn))
+            case "7":
+                isbn = input("Enter ISBN of the book to return: ")
+                print(library.return_book(isbn))
+            case "8":
+                print("Instructions on how to use the library manager.")
+            case "9":
+                print("Book System Manager v1.0. Created by Your Name.")
+            case _:
+                print("Invalid choice. Please try again.")
+        input("press Enter")
+        os.system("clear")
 
 if __name__ == "__main__":
-    library = Library()
-    library.load_from_file("library_data.json")
-
-    # Adding books
-    book1 = Book("Harry Potter", "J.K. Rowling", 1997, "1234567890", "harry-potter.txt")
-    book2 = Book("The Hobbit", "J.R.R. Tolkien", 1937, "0987654321", "hobbit.txt")
-    library.add_book(book1)
-    library.add_book(book2)
-
-    # Show available books
-    library.show_available_books()
-
-    # Renting books
-    print(library.rent_book("1234567890"))
-
-    # Show available books after renting
-    library.show_available_books()
-
-    # Returning books
-    print(library.return_book("1234567890"))
-
-    # Show available books after returning
-    library.show_available_books()
-
-    # Save the current state of the library to a file
-    library.save_to_file("library_data.json")
+    main()
